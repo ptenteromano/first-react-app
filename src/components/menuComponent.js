34b7f952eaf0e13222e9bcb9ac5856fc3fee6d4d
Philip Tenteromano <tenteromano.p@gmail.com>
2018-07-17 13:01:15 -0400

    image and css bug fix

--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -58,7 +58,7 @@ class Menu extends Component {
         <div key={dish.id} className="col-12 mt-5">
           <Media tag="li">
             <Media left middle>
-              <Media obj src={dish.img} alt={dish.name} />
+              <Media object src={dish.image} alt={dish.name} />
             </Media>
             <Media body className="ml-5">
               <Media heading>{dish.name}</Media>
