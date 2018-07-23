1ca889c2e44ea0ff32192fc9a4ec4b163163fba7
Philip Tenteromano <tenteromano.p@gmail.com>
2018-07-23 14:10:33 -0400

    conversion into functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,66 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
+// functional component - if a component is rendered purely based
+// on props passed to it without changing state
+// can be turned into functional
+// ex: useless constructor
 
-  properDate(dateStr) {
-    var date = new Date(dateStr);
-    return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+// lower case - not a render component
+function properDate(dateStr) {
+  var date = new Date(dateStr);
+  return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+}
+
+// uppercase - render'd component
+function RenderDish({dish}) {
+  if (dish != null) {
+    return (
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
   }
-
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name}/>
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    else {
-      return (
-        <div></div>
-      );
-    }
+  else {
+    return (
+      <div></div>
+    );
   }
+}
 
-  renderComments(comments) {
-    if (comments != null) {
-      return (
-          <div className="mt-1">
-            <h3>Comments</h3>
-            <ul className="list-unstyled">
-              {comments.map((comment) => (
-                <li key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>-- {comment.author}, {this.properDate(comment.date)}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-      );
-    }
-    else {
-      return (
-        <div></div>
-      );
-    }
+function RenderComments({comments}) {
+  if (comments != null) {
+    return (
+        <div className="mt-1">
+          <h3>Comments</h3>
+          <ul className="list-unstyled">
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {properDate(comment.date)}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+    );
   }
+  else {
+    return (
+      <div></div>
+    );
+  }
+}
 
-  render() {
-    if(this.props.dish) {
-      console.log(this.props.dish.comments);
+// arrow syntax, seems like eslint wants semi at end of arrow's
+// calls the other functions bove inside
+const DishDetail = (props) => {
+    if(props.dish) {
+      console.log(props.dish.comments);
       return(
         <div className="container">
           <div className="row">
             <div className="col-12 col-md-5 m-1">
-              {this.renderDish(this.props.dish)}
+              <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-              {this.renderComments(this.props.dish.comments)}
+              <RenderComments comments={props.dish.comments} />
             </div>
           </div>
         </div>
@@ -71,7 +78,6 @@ class DishDetail extends Component {
         <div></div>
       );
     }
-  }
+  };
 
-}
 export default DishDetail;
--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(dish.id)}>
+// functional component to Render Menu Items
+// functions just become another way to create components
+// only two functions here
+function RenderMenuItem({ dish, onClick }) {
+  return(
+    <Card onClick={() => onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay body className="ml-5">
               <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
           </Card>
-        </div>
-      );
-    });
+  );
+}
 
+// arrow function syntax, same functionality as above function def
+// creates a variable with all dish items
+// calls that variable in return statement
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
     return (
-      <div className="container">
-        <div className="row">
-          {menu}
-        </div>
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        {menu}
+      </div>
+    </div>
+  );
+};
 
 // need to export so it can be used
 export default Menu;
