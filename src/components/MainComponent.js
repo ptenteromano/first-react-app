e6c7b3c38e57f1e6fd160956c4aae67e68a526f0
Philip Tenteromano <tenteromano.p@gmail.com>
2018-08-28 15:31:37 -0400

    redux init

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,23 +8,12 @@ import DishDetail from './DishDetailComponent';
 import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
   }
 
   render() {
