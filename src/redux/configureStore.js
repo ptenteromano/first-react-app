3734be19e4a3d09947c9d1efaea02f3d68329366
Philip Tenteromano <tenteromano.p@gmail.com>
2018-09-12 11:55:01 -0400

    separate reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
 
 // store part of flux architecture
 // don't have to follow this parttern (with this file)
 export const ConfigureStore = () => {
   const store = createStore(
-    Reducer,
-    initialState
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
+    })
   );
   return store;
 };
